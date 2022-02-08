0c94222b418bf62d076c6db57b30ac4bab22f950
BhuiyanSaif18 <saif_lesnar@outlook.com>
2022-02-08 12:46:58 +0600

    Gas endpoint refactored with async await

--- routes/gas.js
+++ routes/gas.js
@@ -1,36 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var Web3 = require('web3');
-const fs = require('fs');
 const schedule = require('node-schedule');
 require('dotenv').config();
 const { ethers } = require("ethers");
+const { readFile } = require("../utils/fileSystem");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
 
   
   var web3 = new Web3(new Web3.providers.HttpProvider(process.env.FTM_MAIN_NET));
-  fs.readFile("./gasdata.json", "utf8", (err, jsonString) => {
-    if (err) {
-      console.log("Error reading file from disk:", err);
-      return;
-    }
-    try {
-      gasPrice = JSON.parse(jsonString);
-      // web3.utils.fromWei(gasPrice.gasprice)
-      if(gasPrice.gasprice){
-        res.render('keys/gasprice', {viewTitle : "Gas Price " + ethers.utils.formatUnits(gasPrice.gasprice, "gwei") + " GWei"});
-      }
-      else{
-        res.render('keys/gasprice', {viewTitle : "Gas Price not retrived yet"});
-      }
-      
-    } catch (err) {
-      console.log("Error parsing JSON string:", err);
-    }
-  });
-   
+
+  var data = await readFile('./gasdata.json');
+  var gasPrice = JSON.parse(data).gasprice;
+
+  if(gasPrice){
+    res.render('keys/gasprice', {viewTitle : "Gas Price " + ethers.utils.formatUnits(gasPrice, "gwei") + " GWei"});
+  }
+  else{
+    res.render('keys/gasprice', {viewTitle : "Gas Price not retrived yet"});
+  }
 });
 
 
