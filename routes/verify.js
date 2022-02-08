816c970660c68fb19f41cc65df6e1726b14070c5
BhuiyanSaif18 <saif_lesnar@outlook.com>
2022-02-08 14:56:16 +0600

    Refactored with async await on verify route

--- routes/verify.js
+++ routes/verify.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const fs = require('fs');
 var request = require('request');
 require('dotenv').config();
+const { readFile, writeFile } = require("../utils/fileSystem");
 
 var transactRouter = require('./transact');
 
@@ -13,50 +14,25 @@ router.get('/', async function(req, res, next) {
 
 
     if(web3.utils.isAddress(address)){
-      fs.readFile("./customer.json", "utf8", (err, jsonString) => {
-        if (err) {
-          console.log("Error reading file from disk:", err);
-          return;
-        }
-        try {
-          customer = JSON.parse(jsonString);
-          console.log("Customer address is:", customer);
-          
-          if(!customer.WhiteList.includes( address)){
-            customer.WhiteList.push( address)
-            const outputjsonString = JSON.stringify(customer)
-            fs.writeFile('./customer.json', outputjsonString, err => {
-                if (err) {
-                    console.log('Error writing file', err)
-                } else {
-                    console.log('Successfully wrote file')
+      var CustomersStringifyData = await readFile('./customer.json');
+      customerData = JSON.parse(CustomersStringifyData);
+        console.log("Customer address is:", customerData);
+        
+        if(!customerData.WhiteList.includes(address)){
+          customerData.WhiteList.push( address)
+          const outputjsonString = JSON.stringify(customerData);
+          await writeFile('./customer.json', outputjsonString)
+          var url = process.env.FTM_TEST_NET_API_URL +'?module=account&action=txlist&address='+address+'&startblock=0&endblock=99999999&sort=asc&apikey='+process.env.FTM_API_KEY;
+          request.get(url, async function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              result = JSON.parse(response.body).result;
+                console.log( result.length);
+                if(result.length == 0){
+                  transactRouter.transaction(address);
                 }
-            })
-          }
-          else {
-            console.log("prints else");
-          }
-          
-          
-        } catch (err) {
-          console.log("Error parsing JSON string:", err);
+            }
+          });
         }
-      });
-      var url = process.env.FTM_TEST_NET_API_URL +'?module=account&action=txlist&address='+address+'&startblock=0&endblock=99999999&sort=asc&apikey='+process.env.FTM_API_KEY;
-      request.get(url, async function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            result = JSON.parse(response.body).result;
-              console.log( result.length);
-              if(result.length == 0){
-                transactRouter.transaction(address);
-              }
-          }
-      });
-      // web3.eth.getTransactionCount(address).then(result => {
-      //   if(parseInt(result) > 0){
-      //     transactRouter.transaction(address);
-      //   }
-      // })
     }
     res.render("keys/add", {
       viewTitle: address? web3.utils.isAddress(address)? "Valid Address Added" : "Invalid Address" :  "Insert Public Key"
@@ -66,27 +42,12 @@ router.get('/', async function(req, res, next) {
 
 
 
-router.get('/list', (req, res) => {
-  var publist = [];
-  fs.readFile("./customer.json", "utf8", (err, jsonString) => {
-        if (err) {
-          console.log("Error reading file from disk:", err);
-          return;
-        }
-        try {
-          customer = JSON.parse(jsonString);
-          publist = customer.WhiteList;
-          console.log("Customer address is:", publist);
-          res.render("keys/list", {
-            list: publist
-          });
-          
-        } catch (err) {
-          console.log("Error parsing JSON string:", err);
-        }
-      });
-  
-  
+router.get('/list',async  (req, res) => {
+  var CustomersStringifyData = await readFile('./customer.json');
+  var publist = JSON.parse(CustomersStringifyData).WhiteList;
+  res.render("keys/list", {
+    list: publist
+  });
 });
 
 
