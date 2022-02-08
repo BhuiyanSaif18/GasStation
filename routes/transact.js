9db8ea80beaf3e32469a7fd539aaf8881fc038aa
BhuiyanSaif18 <saif_lesnar@outlook.com>
2022-02-08 14:31:19 +0600

    Transaction route refactored

--- routes/transact.js
+++ routes/transact.js
@@ -3,50 +3,30 @@ require('dotenv').config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 var express = require('express');
 var router = express.Router();
-const fs = require('fs');
+const { readFile } = require("../utils/fileSystem");
 
-
-
-
-
-
-function transaction (req) {
-    let receiptAddress = req;
-    console.log(req);
-    const myPrivateKeyHex = process.env.DAPPPRIVATEKEY;
-    let provider = new HDWalletProvider(myPrivateKeyHex, process.env.FTM_TEST_NET);
-    const localKeyProvider = new HDWalletProvider({
-        privateKeys: [myPrivateKeyHex],
-        providerOrUrl: provider,
-    });
-    const web3 = new Web3(localKeyProvider);
-    fs.readFile("./gasdata.json", "utf8", (err, jsonString) => {
-        if (err) {
-          console.log("Error reading file from disk:", err);
-          return;
-        }
-        try {
-          gasPrice = JSON.parse(jsonString);
-          console.log(gasPrice);
-
-            web3.eth.sendTransaction({
-                to: receiptAddress,
-                value: parseInt(gasPrice.gasprice),
-                data: '',
-                from: process.env.DAPPPUBLICKEY 
-            })
-            .on('transactionHash', function(hash){
-                console.log(hash);
-            })
-            .on('error', console.error);
-          
-        } catch (err) {
-          console.log("Error parsing JSON string:", err);
-        }
-    });
-    
-  };
-
-
+async function transaction (req) {
+  let receiptAddress = req;
+  console.log(req);
+  const myPrivateKeyHex = process.env.DAPPPRIVATEKEY;
+  let provider = new HDWalletProvider(myPrivateKeyHex, process.env.FTM_TEST_NET);
+  const localKeyProvider = new HDWalletProvider({
+      privateKeys: [myPrivateKeyHex],
+      providerOrUrl: provider,
+  });
+  const web3 = new Web3(localKeyProvider);
+  var gasData = await readFile('./gasdata.json');
+  var gasPrice = JSON.parse(gasData).gasprice;
+  web3.eth.sendTransaction({
+    to: receiptAddress,
+    value: parseInt(gasPrice),
+    data: '',
+    from: process.env.DAPPPUBLICKEY 
+  })
+  .on('transactionHash', function(hash){
+      console.log(hash);
+  })
+  .on('error', console.error);
+};
 
 module.exports = {transaction};
